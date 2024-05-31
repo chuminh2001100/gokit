d16008bd1aeec81ee6250f640b3768b77b85ef09
chuminh2001 <chuminh2001100@gmail.com>
2024-05-31 14:17:06 +0700

    all project

---
+++ Grpc_Client/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"google.golang.org/grpc"
+
+	pb "HelloService" // Import generated code
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewHelloServiceClient(conn)
+
+	name := "API kafka"
+	response, err := c.SayHello(context.Background(), &pb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", response.Message)
+}
---
+++ go.mod
@@ -0,0 +1,18 @@
+module helloService
+
+go 1.22.0
+
+require (
+	github.com/go-kit/kit v0.13.0
+	google.golang.org/grpc v1.63.2
+	google.golang.org/protobuf v1.34.1
+)
+
+require (
+	github.com/go-kit/log v0.2.0 // indirect
+	github.com/go-logfmt/logfmt v0.5.1 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240227224415-6ceb2ff114de // indirect
+)
